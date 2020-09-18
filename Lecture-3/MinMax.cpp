7e0f45e9e7f7bec11f27eabd8e0e9de52020858b
Kartik Mathur <kartik.mathur10@gmail.com>
2020-09-18 11:19:38 +0530

    Lecture-3

---
+++ Lecture-3/MinMax.cpp
@@ -0,0 +1,36 @@
+// MinMax
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+	
+	int mi = INT_MAX;
+	int mx = INT_MIN;
+	int sum = 0;
+
+	int n,no;
+	cin>>n;
+
+	int i = 1;
+	while(i<=n){
+		cin>>no;
+
+		if(no<mi){
+			mi = no;
+		}
+		if(no>mx){
+			mx = no;
+		}
+		sum = sum + no;
+
+		i = i + 1;
+	}
+
+	cout<<"Minimum value : "<<mi<<endl;
+	cout<<"Maximum value : "<<mx<<endl;
+	cout<<"Mean value : "<<sum/n<<endl;
+
+	cout<<endl;
+	return 0;
+}
