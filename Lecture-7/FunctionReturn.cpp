1347b8c371e10d91261ca6c12090409f57e5e040
Kartik Mathur <kartik.mathur10@gmail.com>
2020-09-24 13:39:12 +0530

    Lecture-7

---
+++ Lecture-7/FunctionReturn.cpp
@@ -0,0 +1,64 @@
+// FunctionReturn
+#include <iostream>
+using namespace std;
+
+int mulitply(int a,int b){
+	int ans = a*b;
+	return ans; // this is how we can return the value from the function using return
+}
+
+void CheckPrime(int no){
+	int i = 2; 
+	while(i<no){
+
+		if(no%i == 0){
+			cout<<"Not Prime"<<'\n'; 
+			break;
+		}
+
+		i = i + 1;
+	}
+	if( i == no){
+		cout<<"Prime"<<endl;
+	}
+
+	return; // this means function will return back and it is not returning anything
+
+	cout<<"My World!";
+}
+
+void PrintPrimes(int n){
+	int no = 2;
+	while(no<=n){
+		// if no is prime print it, else skip and move to next no
+		int i = 2; 
+		int flag = 1;
+		while(i<no){
+			if(no%i == 0){
+				flag = 0;
+			}
+			i = i + 1;
+		}
+		if(flag == 1){
+			cout<<no<<' ';
+		}
+		no = no+1;
+	}
+	return;
+}
+
+
+int main(){
+	
+	int a=10,b=20;
+	// cin>>a>>b;
+	int ans = mulitply(a,b);
+
+	cout<<ans + 200<<endl;
+
+	CheckPrime(11);
+
+	PrintPrimes(50);
+	cout<<endl;
+	return 0;
+}
--- Lecture-7/Functions.cpp
+++ Lecture-7/Functions.cpp
@@ -1,1 +1,65 @@
-Functions
+// Functions
+#include <iostream>
+using namespace std;
+// Syntax to create a function :
+// datatype func_name(){
+	//task
+// }
+
+void printHello(){
+	cout<<"Hello World!"<<endl;
+}
+
+void oddEven(int n){
+	if(n%2 == 0){
+		cout<<"Even"<<endl;
+	}
+	else{
+		cout<<"Odd"<<endl;
+	}
+}
+
+void multiply(int a,int b){
+	cout<<a*b<<endl;
+}
+
+void FarToCel(int init,int fval,int step){
+
+	int farehn = init;
+	while(farehn<=fval){
+
+		int cel = (5.0/9)*(farehn-32);
+		cout<<farehn<<" "<<cel<<endl;
+
+		farehn = farehn + step;
+	}	
+	cout<<endl;
+}
+
+int main(){
+	
+	printHello(); // Calling or invoking the function
+	
+	int n = 5;
+	// cin>>n;
+	oddEven(n);
+	
+	int a=10,b=40;
+	// cout<<"Enter a and b to multiply : ";
+	// cin>>a>>b;
+	multiply(a,b); 
+
+
+	int init = 0,fval = 300,step = 20;
+	FarToCel(init,fval,step);
+
+	init = 0;fval=300,step = 30;
+	FarToCel(init,fval,step);
+
+	// we might have to use the division output inside of main
+
+
+
+	cout<<endl;
+	return 0;
+}
