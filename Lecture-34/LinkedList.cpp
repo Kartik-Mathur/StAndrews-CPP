8b820f280e1a3bd551f6c8dd66cba83f9b5a4f63
Kartik Mathur <kartik.mathur10@gmail.com>
2020-12-18 13:05:17 +0530

    Lecture-34

--- Lecture-34/LinkedList.cpp
+++ Lecture-34/LinkedList.cpp
@@ -74,6 +74,96 @@ void PrintLL(node* head){
 	cout<<"NULL"<<endl;
 }
 
+void ReverseLL(node* &head,node* &tail){
+	node* prev = NULL,*curr = head,*n;
+	tail = head;
+	while(curr){
+		n = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr  = n;
+	}
+	head = prev;
+}
+
+node* mid(node* head){
+	if(head == NULL || head->next == NULL){
+		return head;
+	}
+
+	node* fast = head->next;
+	node* slow = head;
+	while(fast!=NULL && fast->next!=NULL){
+		fast=fast->next->next;
+		slow=slow->next;
+	}
+	return slow;
+}
+
+void DeleteStartLL(node* &head,node* &tail){
+	if(head == NULL) return;
+	if(head->next == NULL){
+		delete head;
+		head = tail = NULL;
+	}
+	else{
+		node* temp = head;
+		head= head->next;
+		delete temp;
+	}
+}
+
+void DeleteEndLL(node* &head,node* &tail){
+	if(head == NULL) return;
+	if(head->next == NULL){
+		delete head;
+		head = tail = NULL;
+	}
+	else{
+		node* temp = head;
+		while(temp->next!=tail){
+			temp = temp->next;
+		}
+		delete tail;
+		tail = temp;
+		tail->next = NULL;
+	}
+}
+
+void DeleteAtMid(node* &head,node* &tail,int pos){
+	if(pos == 0){
+		DeleteStartLL(head,tail);
+	}
+	else if(pos>=length(head)){
+		DeleteEndLL(head,tail);
+	}
+	else{
+		node* temp = head;
+		int cnt = 1;
+		while(cnt<=pos-1){
+			temp = temp->next;
+			cnt++;
+		}
+		node* x = temp->next;
+		temp->next = x->next;
+		delete x;
+	}
+}
+
+node* SearchRec(node* head,int key){
+	// base case
+	if(head == NULL){
+		return NULL;
+	}
+
+	// recursive case
+	if(head->data == key){
+		return head;
+	}
+	node* ans = SearchRec(head->next,key);
+	return ans;
+}
+
 int main(){
 	#ifndef ONLINE_JUDGE
 	freopen("input.txt","r",stdin);
@@ -103,6 +193,26 @@ int main(){
 
 	InsertLLMid(head,tail,7,3);
 	PrintLL(head);
+
+	node* ans = SearchRec(head,30);
+	if(ans){
+		cout<<"Found "<<ans->data<<endl;
+	}
+	else{
+		cout<<"Not Found"<<endl;
+	}
+
+	PrintLL(head);
+	ReverseLL(head,tail);
+	PrintLL(head);
+	DeleteEndLL(head,tail);
+	PrintLL(head);
+	ans = mid(head);
+	DeleteAtMid(head,tail,100);
+	PrintLL(head);
+	if(ans){
+		cout<<"Mid element is "<<ans->data<<endl;
+	}
 
 	return 0;
 }
